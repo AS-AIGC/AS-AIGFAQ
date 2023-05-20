ef997dfdb5ff9fcc650fa5bdde4cba9a9c0bd3c8
Ling-Jyh Chen <cclljj@gmail.com>
2023-05-20 14:09:48 +0800

    Update AS-AIGFAQ-to-HTML.py

--- AS-AIGFAQ-to-HTML.py
+++ AS-AIGFAQ-to-HTML.py
@@ -1,31 +1,40 @@
+"""
+這段程式碼用於根據提供的設定，從 CSV 檔案中讀取 FAQ 資料並生成對應的 HTML 文件。
+程式使用 pandas 套件來讀取和處理資料，然後根據 FAQ 資料動態生成 HTML 內容，
+最後將 HTML 內容寫入到檔案中。
+
+This code snippet is used to read FAQ data from CSV files and generate 
+corresponding HTML files based on the provided configuration. 
+The code uses the pandas library to read and process the data, and 
+dynamically generates HTML content based on the FAQ data. 
+Finally, it writes the HTML content to files.
+"""
 #!/usr/bin/env python3
+
 import pandas as pd
 import config
 
-SOURCES = {
-        ## fname : HTML_title
-        #'AS-ITS' : '中央研究院 資訊服務 FAQ',
-        }
-
 SOURCES = config.SOURCES
-
 LANGUAGES = config.LANGUAGES
 LANGUAGES.append("")
 
+# Loop through each source
 for name in SOURCES:
     print("Dealing with " + name)
 
     HTML_title = SOURCES[name]
     name = "/home/ec2-user/OpenAI/AS-AIGFAQ/output/" + name
 
+    # Loop through each language
     for lang in LANGUAGES:
-        if lang=="":
+        if lang == "":
             faq_name = name + '-QA.csv'
             html_name = name + '-QA.html'
         else:
             faq_name = name + '-QA_' + lang + '.csv'
             html_name = name + '-QA_' + lang + '.html'
-            
+
+        # Read the FAQ data from the CSV file
         df = pd.read_csv(faq_name)
         df.dropna(inplace=True)
 
@@ -33,8 +42,9 @@ for name in SOURCES:
         for index, row in df.iterrows():
             if row.category not in FAQ:
                 FAQ[row.category] = {}
-            FAQ[row.category]["["+row.title+"] " +row.question] = row.answer
+            FAQ[row.category]["["+row.title+"] " + row.question] = row.answer
 
+        # Generate the HTML content
         html = '<html><head>'
         html += '<meta charset="utf-8"><script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>'
         html += '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">'
@@ -49,14 +59,14 @@ for name in SOURCES:
         num = 1
         for category in FAQ:
             html += '<div class="card">'
-            if num==1:
+            if num == 1:
                 html += '<div class="card-header" id="heading-'+str(num)+'"><h5 class="mb-0"><button class="btn btn-link" data-toggle="collapse" data-target="#collapse-'+str(num)+'" aria-expanded="true" aria-controls="collapse-'+str(num)+'">'
             else:
                 html += '<div class="card-header" id="heading-'+str(num)+'"><h5 class="mb-0"><button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapse-'+str(num)+'" aria-expanded="false" aria-controls="collapse-'+str(num)+'">'
             html += "<h2>" + category + "</h2>"
             html += '</button></h5>'
             html += '</div>'
-            if num==1:
+            if num == 1:
                 html += '<div id="collapse-'+str(num)+'" class="collapse show" aria-labelledby="heading-'+str(num)+'" data-parent="#accordion">'
             else:
                 html += '<div id="collapse-'+str(num)+'" class="collapse" aria-labelledby="heading-'+str(num)+'" data-parent="#accordion">'
@@ -76,8 +86,7 @@ for name in SOURCES:
         html += '</div>'
         html += '</body></html>'
 
+        # Write the HTML content to a file
         with open(html_name, 'w') as writefile:
             writefile.write(html)
             writefile.close()
-
-
